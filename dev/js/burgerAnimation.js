389bf8501bba5fef78333b3a2e8dccfd174dbc56
JulesSC <81724365+JulesSC@users.noreply.github.com>
2022-01-30 22:19:28 -0500

    getting there

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -6,11 +6,11 @@ gsap.registerPlugin(DrawSVGPlugin);
 gsap.set(".burger-lines", {transformOrigin:"center"});
 
 const topTL = new gsap.timeline();
-topTL.to(".burger-lines:nth-child(1)", {duration: 0.25, y:"+=8"})
+topTL.to(".burger-lines:nth-child(1)", {duration: 0.25, y:"+=8", fill: "rgba(56, 17, 19, 1)"})
 .to(".burger-lines:nth-child(1)", {duration: 0.25, rotation: 45});
 
 const bottomTL = new gsap.timeline();
-bottomTL.to(".burger-lines:nth-child(2)", {duration: 0.25, y:"-=8"})
+bottomTL.to(".burger-lines:nth-child(2)", {duration: 0.25, y:"-=8", fill: "rgba(56, 17, 19, 1)"})
 .to(".burger-lines:nth-child(2)", {duration: 0.25, rotation: -45});
 
 export const burgerTL = new gsap.timeline({paused:true});
