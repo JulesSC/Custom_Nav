2e749bfcafd4ff50130c1e7c8fad389c21a30fca
JulesSC <81724365+JulesSC@users.noreply.github.com>
2022-02-07 10:43:25 -0500

    fireside help

--- dev/js/menu.js
+++ dev/js/menu.js
@@ -13,17 +13,31 @@ export function menuListners (){
 
     console.log("working");
 
+    var linkContainer = document.querySelectorAll(".text-container");
+
     links.forEach((link, i) => {
         link.addEventListener("mouseenter", ()=>{
             console.log("mouse enter");
-            gsap.to(links[i],{duration:0.25, fontWeight:700, y:"-=80"});
-            // gsap.from(links[i] chars,{duration:0.25, fontWeight:700, y:"-=80"});
+            
+
+            let newText = document.createElement("h4");
+            newText.innerHTML = " HEllO";
+            newText.classList.add("link");
+            linkContainer[i].appendChild(newText);
+
+            gsap.to("h4.link",{duration:0.25, fontWeight:700, y:"-=80", onComplete: destoryTopLink});
+            
+            gsap.from(links[i] chars,{duration:0.25, fontWeight:700, y:"-=80"});
         })
 
         link.addEventListener("mouseleave", ()=>{
             console.log("mouse leave");
-            gsap.to(links[i],{duration:0.25, fontWeight:300, y:"-=80"});
+            // gsap.to(links[i],{duration:0.25, fontWeight:300, y:"-=80"});
         })
     });
+
+    function destoryTopLink(){
+        console.log("destory");
+    }
 
 }
